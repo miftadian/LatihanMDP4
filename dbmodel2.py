9f8240a8a0c3b4c255185e1542a5124b1913b82e
miftadian <miftadianp@gmail.com>
2025-04-24 09:01:51 +0700

    LatihanMDP4

---
+++ dbmodel2.py
@@ -0,0 +1,17 @@
+import streamlit as st
+import joblib
+import numpy as np
+
+#Memuat model regresi linear yang sudah disimpan
+lin_reg_loaded = joblib.load('lin_reg_model.joblib')
+
+#judul aplikasi
+st.title("Prediksi Salary Berdasarkan Years of Experience")
+
+#Input Years of Experience
+Years_of_Experience = st.number_input("Masukkan Years of Experience:", min_value=0.0, step=0.1)
+
+#Prediksi Salary
+if st.button("Prediksi Salary"):
+    Salary= lin_reg_loaded.predict([[Years_of_Experience]])
+    st.write(f"Salary seseorang setelah bekerja selama {Years_of_Experience} tahun adalah ${Salary[0]:,.2f}")
